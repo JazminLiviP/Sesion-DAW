18690d53fd3b0e5e63aee39dc307fcc039a0c644
JazminliviP <Jazminliviapoma1999@gmail.com>
2019-09-23 12:34:16 -0500

    archivos

---
+++ appSpring/src/java/Controler/Controlador.java
@@ -0,0 +1,13 @@
+
+package Controler;
+
+import config.Conexion;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+
+public class Controlador {
+    Conexion conn= new Conexion();
+    JdbcTemplate JdbcTemplate =new JdbcTemplate();
+    ModelAndView mav=new ModelAndView();
+}
---
+++ bd.sql
@@ -0,0 +1,7 @@
+create database registro;
+create table persona(
+id int primary key auto_increment,
+nombre  varchar(50),
+correo varchar(100),
+nacionalidad varchar(50)
+ )
